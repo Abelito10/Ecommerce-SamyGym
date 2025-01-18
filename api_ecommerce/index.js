a3250422220aabdca36adabf14f11f5849dd0b47
abelito10 <calderonabel20@gmail.com>
2025-01-18 09:23:52 -0500

    correcion de usuarios, categorias y agregar productos

--- api_ecommerce/index.js
+++ api_ecommerce/index.js
@@ -1,30 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import mongoose from 'mongoose';
-import router from './router';
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import mongoose from 'mongoose'
+import router from './router'
 
-// CONEXIÓN A LA BASE DE DATOS
+//CONEXION A LA BASE DE DATOS
+
 mongoose.Promise = global.Promise;
-const dbUrl = "mongodb://localhost:27017/ecommerce_samygym";
-
-mongoose.connect(dbUrl)
-    .then(() => console.log("CONECTADO A LA BD EN EL PUERTO 27017"))
-    .catch(err => console.error("ERROR AL CONECTARSE A LA BD:", err));
+const dbUrL = "mongodb://localhost:27017/ecommerce_samygym";
+mongoose.connect(
+    dbUrL , {
+        useNewUrlParser:  true,
+        useUnifiedTopology: true
+    }
+).then(mongoose => console.log("CONECTADO A LA BD EN EL PUERTO 27017"))
+.catch(err => console.log(err));
 
 const app = express();
-
-// Configuración de middleware
 app.use(cors());
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/', router);
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'public')))
+app.use('/api/',router)
 
-// Configuración del puerto
-app.set('port', process.env.PORT || 3000);
+app.set('port',process.env.PORT || 3000);
 
-// Inicio del servidor
 app.listen(app.get('port'), () => {
-    console.log("EL SERVIDOR SE EJECUTÓ PERFECTAMENTE EN EL PUERTO", app.get('port'));
-});
+    console.log("EL SERVIDOR SE EJECUTO PERFECTAMENTE EN EL PUERTO 3000");
+})
